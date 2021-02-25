889da510a257199ddd296bf5e7ffb502657a5a15
fithawi ghide <lgfetah@gmail.com>
2021-02-25 11:05:37 +0100

    ex-1 done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,10 @@
 // Example
-window.alert("Hello World!");
+//"You will be a X in Y, and married to Z with N kids."
+var nameOfUser;
+var numberOfChildren = 4;
+var partnerName = "Adiam";
+var geographicalLocation = "Asmara";
+var jobTitle = "Doctor";
+nameOfUser = prompt("Enter Your Name to know your fortune");
+window.alert("Hi " +nameOfUser+", "+"You will be a "+jobTitle +" in "+geographicalLocation+
+", "+ "and married to "+ partnerName+" with "+numberOfChildren+" kids" );

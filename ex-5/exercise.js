dc44935358b82a78ae082001cf618fbf0f5ec065
fithawi ghide <lgfetah@gmail.com>
2021-02-25 17:03:11 +0100

    EX -5 done

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -1,2 +1,13 @@
 // Example
-window.alert("Hello World!");
+var str = prompt("Enter your first String");
+var leng1 = str.length;
+//window.alert(leng1);
+//comparing two strings
+var str2 = prompt("Enter your second string");
+var leng2 = str2.length;
+
+if(leng1 > leng2){
+    window.alert(str +" phrase was the longest with "+ leng1 +" number "+ " of characters" )
+}else{
+    window.alert(str2 +" phrase was the longest with "+ leng2 +" number "+ " of characters")
+}

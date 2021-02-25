6253877ff2a07d291f5c9660aba179227470daef
fithawi ghide <lgfetah@gmail.com>
2021-02-25 16:39:29 +0100

    Ex-3 done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,10 @@
 // Example
-window.alert("Hello World!");
+var num1 = parseInt(prompt("Enter first number"));
+var num2 = parseInt(prompt("Enter second number"));
+if(num1 > num2){
+    window.alert("The greater number of " +num1+", "+num2+" is "+num1);
+}else if(num2 > num1){
+    window.alert("The greater number of " +num1+", "+num2+" is "+num2);
+}else{
+    window.alert("The number are same");
+}

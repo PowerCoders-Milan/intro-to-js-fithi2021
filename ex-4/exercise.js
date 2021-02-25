d2dc84d8893e0ab4c2e06d45c0a19cb9d913089d
fithawi ghide <lgfetah@gmail.com>
2021-02-25 16:52:13 +0100

    EX-4 done

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,16 @@
 // Example
-window.alert("Hello World!");
+var myName = prompt("Enter your name");
+var yourDay = prompt("how was your day");
+switch(yourDay){
+    case "Good":
+        //console.log("I'm happy for you " +myName);
+        window.alert("I'm happy for you " +myName);
+        break;
+    case "Bad":
+       // console.log("It is too bad,tomorow will be fine");
+       window.alert("It is too bad,tomorow will be fine");
+       break;
+    default:
+        window.alert("I can't say anything sorry!");     
+        break;   
+}
